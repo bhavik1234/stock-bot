7948840e4c4cea1617dabb5bead70ebdf7cb11ca
bhavik1234 <bhavikchandora1234@gmail.com>
2017-10-25 11:21:45 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,12 @@
+var data=[{
+    product_id:"Qw12343",
+    name:"phone",
+    qty:5
+},
+    {
+        product_id: "tr12343",
+        name: "car",
+        qty: 10
+    }]
+
+module.exports=data;
