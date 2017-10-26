b48bbaa5d48b0d61a4ba15fbc29b34990ca149d7
bhavik1234 <bhavikchandora1234@gmail.com>
2017-10-26 10:30:05 +0530

    updated data

--- data.js
+++ data.js
@@ -1,12 +1,29 @@
-var data=[{
-    product_id:"Qw12343",
-    name:"phone",
-    qty:5
-},
-    {
-        product_id: "tr12343",
-        name: "car",
-        qty: 10
-    }]
+var data = [{
+    product_id: "phone_234",
+    name: "phone",
+    qty: 5,
+    brands: [{
+        brand: "samsung",
+        models:
+        [
+            { model: "Galaxy s7", price: 40000 },
+            { model: "Galaxy NOte 8", price: 6000 }]
 
-module.exports=data;
+    },
+    {
+        brand: "apple",
+        apple: [
+            { model: "iPhone 7", price: 40000 },
+            { model: "iPhone X", price: 80000 }
+        ]
+    }
+    ]
+},
+{
+    product_id: "car_981",
+    name: "car",
+    qty: 10,
+    brands:[]
+}]
+
+module.exports = data;
